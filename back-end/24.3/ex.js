5072aa388dfcc87f51e2d1de00bc7d71228574f7
ALINE <hoshinoaline@yahoo.com.br>
2021-11-22 16:04:10 -0300

    req 5 a 10

--- back-end/24.3/ex.js
+++ back-end/24.3/ex.js
@@ -54,3 +54,50 @@ each: [200, 99, 65]
     }
   }
 );
+
+//ex 6 Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings .
+
+use ('class')
+db.movies.find(
+{ratings:{$elemMatch:{$gt:103}}},
+{ title: true,
+ratings:true,
+_id:false});
+
+//ex 7 Retorne todos os filmes com ratings entre 100 e 105 , exibindo apenas os campos title e ratings .
+use ('class')
+db.movies.find(
+{ratings:{$elemMatch:{$gte:100, $lte: 105}}},
+{ title: true,
+ratings:true,
+_id:false});
+
+//ex 8 Retorne todos os filmes com ratings entre 64 e 105 e divisíveis por 9 , exibindo apenas os campos title e ratings .
+
+use ('class')
+db.movies.find(
+{ratings:{$elemMatch:{$gte:64, $lte: 105,$mod: [9, 0]}}},
+{ title: true,
+ratings:true,
+_id:false});
+
+// ex 9:Retorne os filmes da categoria adventure e com ratings maior do que 103 , exibindo apenas os campos title , ratings e category .
+
+use ('class')
+db.movies.find(
+{ratings:{$elemMatch:{$lt: 103}},
+category:{$all:["adventure"]},
+},
+{ title: true,
+ratings:true,
+category:true,
+_id:false});
+
+//ex 10 Retorne somente o título de todos os filmes com dois elementos no array category .
+
+use ('class')
+db.movies.find(
+{category:{$size:2}},
+{ title: true,
+_id:false});
+

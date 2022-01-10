1084d82a63867abfe560d9497d4a98f13270979f
ALINE <hoshinoaline@yahoo.com.br>
2022-01-10 09:49:46 -0300

    dia 22.2

---
+++ back_end_curriculo_novo/22.2/exercises/index.js
@@ -0,0 +1,34 @@
+function DoMath(a,b,c){
+  return new Promise((resolve,reject)=>{
+    if(typeof a!== 'number' || typeof b!=='number' || typeof c!=='number'){
+      reject('Informe apenas números')
+    }
+    const result = (a +b)* c;
+    if(result < 50){
+      return reject('Valor muito baixo');
+    }
+    resolve(result)
+  })
+}
+
+// Math(1,2,3)
+// .then(resolve=>console.log(resolve))
+// .catch(error=>console.log(error))
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+  function callDoMath() {
+  /* Criamos um novo array de 3 posições
+   * e utilizamos o `map` para gerar um número aleatório
+   * para cada posição do Array
+   */
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+  DoMath(...randomNumbers)
+.then(result => console.log(result))
+.catch(err => console.error(err.message))
+}
+// aqui usando them e catch 
+
+callDoMath()

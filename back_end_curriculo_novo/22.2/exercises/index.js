2f7369efda0ab1f9ed50408a96dc65a6f34980b0
ALINE <hoshinoaline@yahoo.com.br>
2022-01-10 13:58:48 -0300

    exercicio 4

--- back_end_curriculo_novo/22.2/exercises/index.js
+++ back_end_curriculo_novo/22.2/exercises/index.js
@@ -19,16 +19,27 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 100 + 1);
 }
 
-  function callDoMath() {
+  // function callDoMath() {
   /* Criamos um novo array de 3 posições
    * e utilizamos o `map` para gerar um número aleatório
    * para cada posição do Array
    */
-  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
-  DoMath(...randomNumbers)
-.then(result => console.log(result))
-.catch(err => console.error(err.message))
-}
-// aqui usando them e catch 
+//   const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+//   DoMath(...randomNumbers)
+// .then(result => console.log(result))
+// .catch(err => console.error(err.message))
+// }
+// aqui usando then e catch 
 
-callDoMath()
+ async function callDoMath(){
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+  try{
+    const result = await DoMath(...randomNumbers)
+    console.log(result)
+  }
+  catch(err){
+    console.log(err)
+  }
+ }
+
+ callDoMath()
